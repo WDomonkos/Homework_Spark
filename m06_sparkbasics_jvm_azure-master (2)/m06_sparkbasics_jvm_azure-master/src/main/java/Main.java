8957bba876b55443ac643cc1c314ebd8396f5262
WDomonkos <wdomi14@gmail.com>
2024-02-27 13:08:15 +0100

    tried with SparkSession instead, built a package for maven

--- m06_sparkbasics_jvm_azure-master (2)/m06_sparkbasics_jvm_azure-master/src/main/java/Main.java
+++ m06_sparkbasics_jvm_azure-master (2)/m06_sparkbasics_jvm_azure-master/src/main/java/Main.java
@@ -3,7 +3,7 @@ import org.apache.spark.sql.SparkSession;
 public class Main {
 
     public static void main(String[] args) {
-        var session = SparkSession.builder()
+        SparkSession session = SparkSession.builder()
                 .appName("Spark Homework Demo")
                 .master("local[*]")
                 .getOrCreate();
